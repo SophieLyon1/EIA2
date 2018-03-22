b566d9962d97cb746756b69b8c29364c15449365
Annsophie <Annsophie@klauss-mbp.eduroam.hs-furtwangen.de>
2018-03-22 17:37:19 +0100

    Aufgabe00

---
+++ Aufgabe00/aufgabe00.d.ts
@@ -0,0 +1,1 @@
+declare function chatWindow(): void;
---
+++ Aufgabe00/aufgabe00.ts
@@ -0,0 +1,23 @@
+ /* 
+Aufgabe 00: Miniseite
+Name: Annsophie Rösch
+Matrikel: 257727
+Datum: 22.Mrz18
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. 
+*/
+
+
+
+
+
+
+function chatWindow() {
+    var eingabe = prompt("Wie heißt du?", "");
+    if (eingabe != null) {
+        document.getElementById("HuHu").innerHTML =
+            "Herzlich Wilkommen, " + eingabe + "! Ich wünsche dir einen schönen Tag";
+    }
+}
